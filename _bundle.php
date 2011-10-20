911c3ac99f5a6b37e53aa18f8bcd74c5b9de16fd
Kelly Becker <kellylsbkr@gmail.com>
2011-10-20 13:20:22 -0400

    save

--- _bundle.php
+++ _bundle.php
@@ -9,19 +9,17 @@ use Exception;
 
 class Bundle {
 	
-	public static $spyc;
-	
-	public function __construct() {
-		self::$spyc = new Spyc;
-	}
-	
 	public static function load($string, $file = false) {
-		if($file) return self::$spyc->loadFile($string);
-		return self::$spyc->load($string);
+		$spyc = new Spyc;
+		
+		if($file) return $spyc->loadFile($string);
+		return $spyc->load($string);
 	}
 	
 	public static function dump($array) {
-		return self::$spyc->dump($array);
+		$spyc = new Spyc;
+				
+		return $spyc->dump($array);
 	}
 	
 }
